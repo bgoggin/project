82077acd0b3ca596f19f59ce9df82525e072b2dd
Bill Goggin <bgoggin@mac.com>
2019-03-31 15:06:20 -0400

    initial check in

---
+++ src/main/java/com/example/project/view/HelloWorld.java
@@ -0,0 +1,26 @@
+package com.example.project.view;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named @RequestScoped
+public class HelloWorld {
+	private String input;
+	private String output;
+	
+	public void submit() {
+		output = "Hello world! You just typed: " + input;
+	}
+	
+	public String getInput() {
+		return input;
+	}
+	
+	public void setInput(String input) {
+		this.input = input;
+	}
+	
+	public String getOutput() {
+		return output;
+	}
+}
